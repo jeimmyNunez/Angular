90d66befc1d090d94b59952257960c8b5c4072fc
jeimmyNunez <jeimmyeve27@gmail.com>
2023-06-28 13:55:56 -0500

    Cargando Proyecto

---
+++ 02-gifApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+ 
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    GifsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
